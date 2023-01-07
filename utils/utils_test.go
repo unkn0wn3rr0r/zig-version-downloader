cada94a6366b436dbb9ac15fe902de7b22687eef
Kaloyan Stoyanov <kstoyanovv@abv.bg>
2023-01-07 16:36:25 +0200

    refactor and add some tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,1 +1,39 @@
 package utils_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/unkn0wn3rr0r/zig-version-downloader/utils"
+)
+
+func TestCreateFilePath(t *testing.T) {
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+
+	table := []struct {
+		filename string
+	}{
+		{"utils.go"},
+		{"somepdf.pdf"},
+		{"text.txt"},
+		{"somefolder/text.txt"},
+		{"somefolder/somenestedfolder/text.txt"},
+		{"somefolder\\text.txt"},
+		{"somefolder\\somenestedfolder\\text.txt"},
+	}
+
+	for _, data := range table {
+		actual, err := utils.CreateFilepath(data.filename)
+		if err != nil {
+			t.Error(err)
+		}
+		expected := filepath.FromSlash(filepath.Join(dir, data.filename))
+		if expected != actual {
+			t.Errorf("Expected: %v, Actual: %v", expected, actual)
+		}
+	}
+}

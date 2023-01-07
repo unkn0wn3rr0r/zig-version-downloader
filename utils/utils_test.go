d87181b191ecb5ecb5fbdd31b37abe281012201d
Kaloyan Stoyanov <kstoyanovv@abv.bg>
2023-01-07 23:14:51 +0200

    add tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"encoding/json"
+	"net/http/httptest"
 	"os"
 	"path/filepath"
 	"testing"
@@ -8,7 +10,7 @@ import (
 
 func TestCreateFilepath(t *testing.T) {
 	dir, err := os.Getwd()
-	printErr(err, t)
+	printErr(t, err)
 
 	table := []struct {
 		filename string
@@ -24,7 +26,7 @@ func TestCreateFilepath(t *testing.T) {
 
 	for _, data := range table {
 		actual, err := CreateFilepath(data.filename)
-		printErr(err, t)
+		printErr(t, err)
 
 		expected := filepath.FromSlash(filepath.Join(dir, data.filename))
 		if expected != actual {
@@ -35,15 +37,59 @@ func TestCreateFilepath(t *testing.T) {
 
 func TestGetOs(t *testing.T) {
 	actual, err := getOs()
-	printErr(err, t)
+	printErr(t, err)
 
-	if windows != actual && linux != actual && macos != actual {
+	if actual != windows && actual != linux && actual != macos {
 		t.Errorf("Expected one of: %v, Actual: %v", []string{windows, linux, macos}, actual)
 	}
 
 }
 
-func printErr(err error, t *testing.T) {
+func TestGetArch(t *testing.T) {
+	actual, err := getArch()
+	printErr(t, err)
+
+	if actual != "x86_64" && actual != "aarch64" {
+		t.Errorf("Expected one of: %v, Actual: %v", []string{"x86_64", "aarch64"}, actual)
+	}
+
+}
+
+func TestGetOsFileExtension(t *testing.T) {
+	actual, err := GetOsFileExtension()
+	printErr(t, err)
+
+	if actual != ".zip" && actual != ".tar.xz" {
+		t.Errorf("Expected one of: %v, Actual: %v", []string{".zip", ".tar.xz"}, actual)
+	}
+
+}
+
+func TestGetZigLatestVersion(t *testing.T) {
+	expected := "someversion"
+	var payload struct {
+		Master struct {
+			Version string `json:"version"`
+		} `json:"master"`
+	}
+	payload.Master.Version = expected
+
+	out, err := json.Marshal(payload)
+	printErr(t, err)
+
+	recorder := httptest.NewRecorder()
+	recorder.Write(out)
+	res := recorder.Result()
+
+	actual, err := getZigLatestVersion(res)
+	printErr(t, err)
+
+	if actual != expected {
+		t.Errorf("Expected: %v, Actual: %v", expected, actual)
+	}
+}
+
+func printErr(t *testing.T, err error) {
 	if err != nil {
 		t.Error(err)
 	}

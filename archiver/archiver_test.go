865604f401b382bf89d61a3324c06cc7777c340e
kstoyanov <kstoyanovv@abv.bg>
2023-05-14 00:57:00 +0300

    rm unnecessary stuff from archiver test file

--- archiver/archiver_test.go
+++ archiver/archiver_test.go
@@ -3,20 +3,9 @@ package archiver
 import (
 	"reflect"
 	"testing"
-
-	"github.com/unkn0wn3rr0r/zig-version-downloader/utils"
 )
 
 func TestNewArchiver(t *testing.T) {
-	extension, err := utils.GetOsFileExtension()
-	if err != nil {
-		t.Errorf("Expected: %v, Actual: %v", nil, err)
-	}
-
-	if extension != ".zip" && extension != ".tar.xz" {
-		t.Errorf("Expected one of: %v, Actual: %v", []string{".zip", ".tar.xz"}, extension)
-	}
-
 	actual, err := NewArchiver()
 	if err != nil {
 		t.Errorf("Expected: %v, Actual: %v", nil, err)

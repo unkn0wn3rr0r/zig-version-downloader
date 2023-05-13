7ac7d7a6c8051e41ea4ff8506d146105e3da9856
kstoyanov <kstoyanovv@abv.bg>
2023-05-14 02:05:16 +0300

    add test for unzip functionality and update readme

--- README.md
+++ README.md
@@ -6,6 +6,11 @@
 
 
 ### How to use it:
+- You would need to have Go installed.
+- Clone the repo.
+- Follow the below steps to run the program and the tests.
+
+*P.S. Since I'm not a millionaire and don't own a Mac, I haven't been able to test whether the current implementation works for iOS users, sorry.*.
 
 ##### 1. To download the latest archive:
 ```
@@ -15,4 +20,7 @@ go run main.go
 ##### 2. To run all tests in the subpackages from the current package:
 ```
 go test ./...
+
+Because tests are being cached and most probably you would like to see that the unzip functionality works, you can use a flag to uncache them:
+go test -count=1 ./...
 ```
--- archiver/archiver_test.go
+++ archiver/archiver_test.go
@@ -1,9 +1,16 @@
 package archiver
 
 import (
+	"os"
 	"reflect"
 	"testing"
+	"time"
+
+	"github.com/unkn0wn3rr0r/zig-version-downloader/utils"
 )
+
+// that's the name of the file which resides in the 'test-file.zip' file
+const tempTestFileName = "test-file.txt"
 
 func TestNewArchiver(t *testing.T) {
 	actual, err := NewArchiver()
@@ -21,4 +28,34 @@ func TestNewArchiver(t *testing.T) {
 	if actualName != "ZipArchiver" && actualName != "TarArchiver" {
 		t.Errorf("Expected one of: %v, Actual: %v", concreteTypes, actualName)
 	}
+
+	pathToZipFile, err := utils.CreateFilepath("test-files\\test-file.zip")
+	if err != nil {
+		t.Errorf("Expected: %v, Actual: %v", nil, err)
+	}
+
+	_, err = actual.Unzip(pathToZipFile)
+	if err != nil {
+		t.Errorf("Expected: %v, Actual: %v", nil, err)
+	}
+
+	pathToTempFile, err := utils.CreateFilepath(tempTestFileName)
+	if err != nil {
+		t.Errorf("Expected: %v, Actual: %v", nil, err)
+	}
+
+	time.Sleep(time.Second * 2) // that's just to see that the file is unzipped and then deleted by the cleanup function
+
+	tempFileName, err := cleanupTempTestFile(pathToTempFile)
+	if tempFileName != tempTestFileName || err != nil {
+		t.Errorf("Expected: %v, Actual: %v", tempTestFileName, err)
+	}
+}
+
+func cleanupTempTestFile(filepath string) (string, error) {
+	err := os.Remove(filepath)
+	if err != nil {
+		return "", err
+	}
+	return tempTestFileName, nil
 }
